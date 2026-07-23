3802fe532c5ce74d168ea6b8347e1b46b1339315
Rishi Sen <rishisen003@gmail.com>
2026-07-23 21:45:00 +0530

    Time: 0 ms (100%), Space: 11.1 MB (7.91%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,29 +6,54 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack <TreeNode*> st;
-        TreeNode* node = root;
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, 1});
 
-        vector<int> inorder;
-        while(true){
-            if(node != nullptr){
-                st.push(node);
-                node = node->left;
+        vector<int> pre, in, post;
+        if(root == nullptr) return in;
+
+        while (!st.empty()) {
+            auto it = st.top();
+            st.pop();
+
+            // this is part of preorder
+            // increment the 1 into 2
+            // push the left into the stack
+            if (it.second == 1) {
+                pre.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+
+                if (it.first->left != nullptr) {
+                    st.push({it.first->left, 1});
+                }
             }
+
+            // this is part of inorder
+            // increment the 2 into 3
+            // push the right into the stack
+            else if (it.second == 2) {
+                in.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+
+                if (it.first->right != nullptr) {
+                    st.push({it.first -> right, 1});
+                }
+            }
+
+            // dont push it back again
             else{
-                if(st.empty() == true) break;
-                node = st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node = node-> right;
+                post.push_back(it.first -> val);
             }
         }
-        return inorder;
+        return in;
     }
 };
