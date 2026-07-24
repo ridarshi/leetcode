9901846e2a6a25db45035e8e90fa2b62ad05c336
Rishi Sen <rishisen003@gmail.com>
2026-07-24 19:20:29 +0530

    Time: 0 ms (100%), Space: 22 MB (36.69%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,13 +13,35 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (root == NULL)
-            return false;
+        if(!root) return false;
 
-        if (root->left == NULL && root->right == NULL)
-            return targetSum == root->val;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, root -> val});
 
-        return hasPathSum(root->left, targetSum - root->val) ||
-               hasPathSum(root->right, targetSum - root->val);
+        while(!q.empty()){
+            auto [node, sum] = q.front();
+            q.pop();
+
+            if(node -> right == NULL && node -> left == NULL && targetSum == sum){
+                return true;
+            }
+
+            if(node -> left) q.push({node -> left, sum + node -> left -> val});
+            if(node -> right) q.push({node -> right, sum + node -> right -> val});
+        }
+
+        return false;
     }
 };
+
+// Recursion
+// bool hasPathSum(TreeNode* root, int targetSum) {
+//         if(root == NULL)return false;
+
+//         if(!root -> left && !root -> right){
+//             return targetSum == root -> val;
+//         }
+
+//         return hasPathSum(root -> left, targetSum - root -> val) ||
+//                hasPathSum(root -> right, targetSum - root -> val);
+//     }
