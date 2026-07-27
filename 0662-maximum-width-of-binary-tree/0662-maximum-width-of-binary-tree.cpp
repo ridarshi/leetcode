992599bc1395a3a4da734e9fa46a2a64a509dfc5
Rishi Sen <rishisen003@gmail.com>
2026-07-27 13:58:19 +0530

    Time: 3 ms (13.8%), Space: 19.7 MB (45.53%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -6,36 +6,41 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)return 0;
+        if (!root)
+            return 0;
 
         long long ans = 0;
-        queue<pair<TreeNode*, int>> q;
+
+        queue<pair<TreeNode*, long long>> q;
         q.push({root, 0});
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int size = q.size();
+
             long long mini = q.front().second;
             long long first, last;
 
-            for(int i = 0;i<size;i++){
-                long long currentId = q.front().second - mini;
+            for (int i = 0; i < size; i++) {
+                long long curr_id = q.front().second - mini;
                 TreeNode* node = q.front().first;
                 q.pop();
 
-                if(i == 0)first = currentId;
-                if(i == size - 1) last = currentId;
-
-                if(node -> left){
-                    q.push({node -> left, 2*currentId + 1});
+                if (i == 0)
+                    first = curr_id;
+                if (i == size - 1)
+                    last = curr_id;
+                if (node->left) {
+                    q.push({node->left, 2 * curr_id + 1});
                 }
-                if(node -> right){
-                    q.push({node -> right, 2*currentId + 2});
+                if (node->right) {
+                    q.push({node->right, 2 * curr_id + 2});
                 }
             }
             ans = max(ans, last - first + 1);
