2418079fea3868983e231bd1a60572fa2adae81b
Rishi Sen <rishisen003@gmail.com>
2026-08-09 14:21:31 +0530

    Time: 0 ms (100%), Space: 21.8 MB (20.83%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -10,28 +10,58 @@
  * right(right) {}
  * };
  */
+
+// BRUTE: RECURSION
+// class Solution {
+// public:
+//     int countNode(TreeNode* root, long long targetSum) {
+//         if (!root)
+//             return 0;
+
+//         int count = 0;
+
+//         if (root->val == targetSum) {
+//             count++;
+//         }
+//         count += countNode(root->left, targetSum - root->val);
+//         count += countNode(root->right, targetSum - root->val);
+
+//         return count;
+//     }
+
+//     int pathSum(TreeNode* root, int targetSum) {
+//         if (!root)
+//             return 0;
+//         return countNode(root, targetSum) + pathSum(root->left, targetSum) +
+//                pathSum(root->right, targetSum);
+//     }
+// };
+
 class Solution {
 public:
-    int countNode(TreeNode* root, long long targetSum){
+    int dfs(TreeNode* root, long long currentSum, long long targetSum,
+        unordered_map<long long, int>& mp) {
         if(!root)return 0;
 
-        int count = 0;
+        currentSum += root -> val;
 
-        if(root -> val == targetSum){
-            count++;
-        }
+        int count = mp[currentSum - targetSum];
 
-        count += countNode(root -> left, targetSum - root -> val);
-        count += countNode(root -> right, targetSum - root -> val);
+        mp[currentSum]++;
+
+        count += dfs(root->left, currentSum, targetSum, mp);
+        count += dfs(root->right, currentSum, targetSum, mp);
+
+        mp[currentSum]--;
 
         return count;
     }
 
     int pathSum(TreeNode* root, int targetSum) {
-        if (!root)
-            return 0;
+        unordered_map<long long, int> mp;
 
-        return countNode(root, targetSum) + pathSum(root->left, targetSum) +
-               pathSum(root->right, targetSum);
+        mp[0] = 1;
+
+        return dfs(root, 0, targetSum, mp);
     }
 };
