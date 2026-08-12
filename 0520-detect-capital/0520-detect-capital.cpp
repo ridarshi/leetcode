952d965dbf2194957f903a3594524f4b6a50f296
Rishi Sen <rishisen003@gmail.com>
2026-08-12 10:49:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.36%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -4,20 +4,24 @@ public:
         
         int capital = 0;
 
+        // check for each letter
         for(char c : word){
             if(isupper(c)){
                 capital++;
             }
         }
 
+        // if all are in lower or all are in upper then return true
         if(capital == 0 || capital == word.length()){
             return true;
         }
 
+        // if one is upper and the rest are in lower and the one is in front of the word then return true;
         if(capital == 1 && isupper(word[0])){
             return true;
         }
 
+        //  else return false;
         return false;
     }
 };
