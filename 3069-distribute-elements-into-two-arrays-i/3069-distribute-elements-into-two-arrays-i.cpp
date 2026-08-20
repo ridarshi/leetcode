82ba55e4f7406d318894ccd2a21a2ce912994d30
Rishi Sen <rishisen003@gmail.com>
2026-08-20 20:45:09 +0530

    Time: 0 ms (100%), Space: 23.6 MB (97.31%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -1,24 +1,60 @@
+// class Solution {
+// public:
+//     vector<int> resultArray(vector<int>& nums) {
+//         int n = nums.size();
+
+//         vector<int> arr1, arr2;
+
+//         arr1.push_back(nums[0]);
+//         arr2.push_back(nums[1]);
+
+//         for(int i = 2; i<n;i++){
+//             if(arr1.back() > arr2.back()){
+//                 arr1.push_back(nums[i]);
+//             }
+//             else{
+//                 arr2.push_back(nums[i]);
+//             }
+//         }
+
+//         arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+
+//         return arr1;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> arr1, arr2;
+        vector<int> arr(n);
 
-        arr1.push_back(nums[0]);
-        arr2.push_back(nums[1]);
+        arr[0] = nums[0];
+        arr[n-1] = nums[1];
 
-        for(int i = 2; i<n;i++){
-            if(arr1.back() > arr2.back()){
-                arr1.push_back(nums[i]);
+        // initiating the two pointers
+        int idx = 0;
+        int revIdx = n-1;
+
+        for(int i = 2;i<n;i++){
+            //  checking for the first condition
+            if(arr[idx] > arr[revIdx]){
+                idx++;
+                arr[idx] = nums[i];
             }
+            // checking for the second condition
             else{
-                arr2.push_back(nums[i]);
+                revIdx--;
+                arr[revIdx] = nums[i];
             }
         }
 
-        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        // reversing the second part of the array;
+        reverse(arr.begin()+revIdx, arr.end());
 
-        return arr1;
+        return arr;
     }
 };
