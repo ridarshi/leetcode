66932b4011581a64c57b7836ae7c45cdc7a92f27
Rishi Sen <rishisen003@gmail.com>
2026-09-01 12:11:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (18.01%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,15 +1,44 @@
+// class Solution {
+// public:
+//     bool isPerfectSquare(int num) {
+
+//         for(int i = 1; ;i++){
+//             long long square = 1LL * i * i;
+
+//             if(square == num){
+//                 return true;
+//             }
+//             if(square > num){
+//                 break;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+// OPTIMAL BINARY SEARCH SOLUTION
 class Solution {
 public:
     bool isPerfectSquare(int num) {
+        if (num < 2)return true;
 
-        for(int i = 1; ;i++){
-            long long square = 1LL * i * i;
+        int low = 2;
+        int high = num/2;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            long long square = 1LL*mid*mid;
 
             if(square == num){
                 return true;
             }
-            if(square > num){
-                break;
+            else if(square < num){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
             }
         }
         return false;
