1d9ab74b680e90d955f9bd775e2ae2a5c65325f3
Rishi Sen <rishisen003@gmail.com>
2026-09-04 12:21:02 +0530

    Time: 2 ms (29.25%), Space: 31 MB (66.39%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -8,6 +8,8 @@ public:
         // maximum value from nums[0] to nums[i]
         vector<int> stable(n);
 
+        // take the first value of the given array as the initial (minimum)
+        // value of the stable array
         stable[0] = nums[0];
 
         // Build prefix maximum array
@@ -15,8 +17,7 @@ public:
             stable[i] = max(nums[i], stable[i - 1]);
         }
 
-        // mini will store the minimum value
-        // from the current index i to the end
+        // mini will store the minimum value from the current index i to the end
         int mini = INT_MAX;
 
         // Traverse from right to left
@@ -25,18 +26,13 @@ public:
             // Update suffix minimum
             mini = min(mini, nums[i]);
 
-            // stable[i] currently contains:
-            // maximum from [0 ... i]
-            //
+            // stable[i] currently contains: maximum from [0 ... i]
             // Subtract minimum from [i ... n-1]
-            //
-            // So now:
-            // stable[i] = max(nums[0...i]) - min(nums[i...n-1])
+            // So now: stable[i] = max(nums[0...i]) - min(nums[i...n-1])
             stable[i] -= mini;
         }
 
-        // Find the first index where
-        // the difference is <= k
+        // Find the first index where the difference is <= k
         for (int i = 0; i < n; i++) {
 
             if (stable[i] <= k) {
